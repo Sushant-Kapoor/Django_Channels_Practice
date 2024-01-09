8c4eeb1745d509912a0003f8cee079438ff6000d
Sushant <sushant1999official@gmail.com>
2024-01-10 03:20:45 +0530

    gs8 | Add basic frontend to send messages to backend

--- gs8/app/consumers.py
+++ gs8/app/consumers.py
@@ -1,6 +1,6 @@
 from channels.consumer import SyncConsumer, AsyncConsumer
 from channels.exceptions import StopConsumer
-from asgiref import async_to_sync
+from asgiref.sync import async_to_sync
 
 
 class MySyncConsumer(SyncConsumer):
@@ -20,7 +20,14 @@ class MySyncConsumer(SyncConsumer):
 
     def websocket_receive(self, event):
         print('Message received from client...', event)
+        self.send({
+            'type': 'websocket.send',
+            'text': 'This is from server to client'
+        })
 
     def websocket_disconnect(self, event):
         print('Websocket disconnected...', event)
+        # Dicard channel from the group once it closes
+        async_to_sync(self.channel_layer.group_discard)(
+            'programmers', self.channel_name)
         raise StopConsumer()

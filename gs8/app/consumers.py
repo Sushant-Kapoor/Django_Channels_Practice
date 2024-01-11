51cc7898cde6f48730b3cdad05e5c14630de2fe4
Sushant <sushant1999official@gmail.com>
2024-01-11 23:02:50 +0530

    gs9 | Add code for sending messages to groups in AsyncConsumer

--- gs8/app/consumers.py
+++ gs8/app/consumers.py
@@ -40,3 +40,41 @@ class MySyncConsumer(SyncConsumer):
         async_to_sync(self.channel_layer.group_discard)(
             'programmers', self.channel_name)
         raise StopConsumer()
+
+
+class MyAsyncConsumer(AsyncConsumer):
+    async def websocket_connect(self, event):
+        print('Websocket connected...', event)
+        print('Channel layer...', self.channel_layer)  # default channel layer
+        print('Channel name...', self.channel_name)  # default channel name
+
+        # add channel to new or existing group
+        # group_add is a async function which needs to be converted to sync
+        await self.channel_layer.group_add('programmers', self.channel_name)
+
+        await self.send({
+            'type': 'websocket.accept'
+        })
+
+    async def websocket_receive(self, event):
+        print('Message received from client...', event['text'])
+        print('Type of message received from client', type(event['text']))
+        await self.channel_layer.group_send('programmers', {
+            'type': 'chat.message',
+            'message': event['text']
+        })
+
+    async def chat_message(self, event):
+        print('Event...', event)
+        print('Actual data...', event['message'])
+        await self.send({
+            'type': 'websocket.send',
+            'text': event['message']
+        })
+
+    async def websocket_disconnect(self, event):
+        print('Websocket disconnected...', event)
+        # Dicard channel from the group once it closes
+        await self.channel_layer.group_discard(
+            'programmers', self.channel_name)
+        raise StopConsumer()

3607ffff1bb2ec8780e4d3c10f6fd22da482a814
Sushant <sushant1999official@gmail.com>
2024-01-12 21:06:20 +0530

    gs9 | Make sample chat application that sends data based on group

---
+++ gs9/app/consumers.py
@@ -0,0 +1,84 @@
+from channels.consumer import SyncConsumer, AsyncConsumer
+from channels.exceptions import StopConsumer
+from asgiref.sync import async_to_sync
+
+
+class MySyncConsumer(SyncConsumer):
+    def websocket_connect(self, event):
+        print('Websocket connected...', event)
+        print('Channel layer...', self.channel_layer)  # default channel layer
+        print('Channel name...', self.channel_name)  # default channel name
+
+        self.group_name = self.scope['url_route']['kwargs']['groupName']
+        print('Groupname in consumer...', self.group_name)
+
+        # add channel to new or existing group
+        # group_add is a async function which needs to be converted to sync
+        async_to_sync(self.channel_layer.group_add)(
+            self.group_name, self.channel_name)
+
+        self.send({
+            'type': 'websocket.accept'
+        })
+
+    def websocket_receive(self, event):
+        print('Message received from client...', event['text'])
+        print('Type of message received from client', type(event['text']))
+        async_to_sync(self.channel_layer.group_send)(self.group_name, {
+            'type': 'chat.message',
+            'message': event['text']
+        })
+
+    def chat_message(self, event):
+        print('Event...', event)
+        print('Actual data...', event['message'])
+        self.send({
+            'type': 'websocket.send',
+            'text': event['message']
+        })
+
+    def websocket_disconnect(self, event):
+        print('Websocket disconnected...', event)
+        # Dicard channel from the group once it closes
+        async_to_sync(self.channel_layer.group_discard)(
+            self.group_name, self.channel_name)
+        raise StopConsumer()
+
+
+class MyAsyncConsumer(AsyncConsumer):
+    async def websocket_connect(self, event):
+        print('Websocket connected...', event)
+        print('Channel layer...', self.channel_layer)  # default channel layer
+        print('Channel name...', self.channel_name)  # default channel name
+        self.group_name = self.scope['url_route']['kwargs']['groupName']
+
+        # add channel to new or existing group
+        # group_add is a async function which needs to be converted to sync
+        await self.channel_layer.group_add(self.group_name, self.channel_name)
+
+        await self.send({
+            'type': 'websocket.accept'
+        })
+
+    async def websocket_receive(self, event):
+        print('Message received from client...', event['text'])
+        print('Type of message received from client', type(event['text']))
+        await self.channel_layer.group_send(self.group_name, {
+            'type': 'chat.message',
+            'message': event['text']
+        })
+
+    async def chat_message(self, event):
+        print('Event...', event)
+        print('Actual data...', event['message'])
+        await self.send({
+            'type': 'websocket.send',
+            'text': event['message']
+        })
+
+    async def websocket_disconnect(self, event):
+        print('Websocket disconnected...', event)
+        # Dicard channel from the group once it closes
+        await self.channel_layer.group_discard(
+            self.group_name, self.channel_name)
+        raise StopConsumer()

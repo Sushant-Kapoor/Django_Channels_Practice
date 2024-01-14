05ebb1eeca2ad4b16bfb39a60b59736898bf70e3
Sushant <sushant1999official@gmail.com>
2024-01-14 19:39:18 +0530

    gs10 | Add models to store chat and group names

--- gs10/app/models.py
+++ gs10/app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Chat(models.Model):
+    content = models.CharField(max_length=100)
+    timestamp = models.DateTimeField(auto_now=True)
+    group = models.ForeignKey('Group', on_delete=models.CASCADE)
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=255)

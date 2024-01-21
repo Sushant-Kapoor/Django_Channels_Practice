33e7087868965d035f16b495a86fc2a42cceb391
Sushant <sushant1999official@gmail.com>
2024-01-21 23:47:02 +0530

    gs12 | Finish gs12

--- gs12/app/consumers.py
+++ gs12/app/consumers.py
@@ -4,9 +4,27 @@ from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
 class MyWebsocketConsumer(WebsocketConsumer):
     def connect(self):
         print('Websocket connected...')
+        self.accept()
 
     def receive(self, text_data=None, bytes_data=None):
         print('Message received from client...', text_data)
+        self.send(text_data='Message sent from client')
+        # self.close()
+        # self.close(code=1423)
 
     def disconnect(self, code):
+        print('Websocket disconnected...', code)
+
+
+class MyAsyncWebsocketConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        print('Websocket connected...')
+        await self.accept()
+
+    async def receive(self, text_data=None, bytes_data=None):
+        print('Message received from client...', text_data)
+        await self.send(text_data='Message sent from client')
+        # self.close()
+
+    async def disconnect(self, code):
         print('Websocket disconnected...', code)
--- gs12/app/routing.py
+++ gs12/app/routing.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/wsc/', consumers.MyWebsocketConsumer.as_asgi())
+    path('ws/wsc/', consumers.MyWebsocketConsumer.as_asgi()),
+    path('ws/awsc/', consumers.MyAsyncWebsocketConsumer.as_asgi()),
 ]
